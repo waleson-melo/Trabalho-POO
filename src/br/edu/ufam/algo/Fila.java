65b318eb2e4e1e385d1371788c6cd0fe48a3b04e
Waleson Melo <walesonmelo23@gmail.com>
2021-10-27 18:10:01 -0400

    Criado os metodos da Fila e implementados na Main

--- src/br/edu/ufam/algo/Fila.java
+++ src/br/edu/ufam/algo/Fila.java
@@ -1,8 +1,30 @@
 package br.edu.ufam.algo;
 
+import java.util.LinkedList;
+
 /**
  * @author waleson_melo
  */
 public class Fila {
+    private LinkedList<String> fila = new LinkedList<>();
 
+    public void enfileirar(String valor) {
+        this.fila.add(valor);
+    }
+
+    public void desenfileirar() {
+        this.fila.remove();
+    }
+
+    public void mostrarFila() {
+        System.out.println("\t\t" + this.fila);
+    }
+
+    public void primeiroFila() {
+        System.out.println("\t\t" + this.fila.getFirst());
+    }
+
+    public void ultimoFila() {
+        System.out.println("\t\t" + this.fila.getLast());
+    }
 }
--- src/br/edu/ufam/app/Main.java
+++ src/br/edu/ufam/app/Main.java
@@ -11,7 +11,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		Fila f = new Fila();
-		
+		System.out.println("----------------------------------------------------------");
+		System.out.println("Operações com Fila:");
+
+		System.out.println("\tEnfileirando: ");
+		f.mostrarFila();
+		f.enfileirar("Jordan");
+		f.mostrarFila();
+		f.enfileirar("Mikael");
+		f.mostrarFila();
+		f.enfileirar("Tupã");
+		f.mostrarFila();
+
+		System.out.println("\tPrimeiro e Ultimo da fila respectivamente: ");
+		f.primeiroFila();
+		f.ultimoFila();
+
+		System.out.println("\tDesenfileirando: ");
+		f.mostrarFila();
+		f.desenfileirar();
+		f.mostrarFila();
+		f.desenfileirar();
+		f.mostrarFila();
+		f.desenfileirar();
+		f.mostrarFila();
+		System.out.println("----------------------------------------------------------");
+
 		Pilha p = new Pilha();
 		
 		Lista l = new Lista();
